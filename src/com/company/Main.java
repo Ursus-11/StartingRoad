01b3e7399a2f1e1d9c5e2708031bead6ad6481c0
Ursus-11 <6per.aspera.ad.astra9@gmail.com>
2022-07-27 14:48:21 +0300

    Еще раз не пугайся, провожу исследования

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Arrays;
+import java.util.Scanner;
+class LogicOperations {
+
+     public static void main(String[] args) {
+         Scanner num = new Scanner(System.in);
+         System.out.println("Введите число");
+         int first;
+         first = num.nextInt();
+
+         if (first == 20)
+             System.out.println("Вы ввели 20");
+     }
+     //    System.out.println("Enter second num ");
+      //   second = num.nextInt();
+       //  System.out.println("Your result = ");
+    //     result = first + second;
+   //      result *=(6 -2);
+   //      System.out.print("Your result = " + result);
+
+     }
+
+
+
---
+++ src/com/company/SecondClass.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.Scanner;
+
+public class SecondClass {
+        Scanner num = new Scanner(System.in);
+        int frog;
+
+        @Override
+        public String toString() {
+                return "SecondClass{" +
+                        "num=" + num +
+                        ", frog=" + frog +
+                        '}';
+        }
+
+
+}
