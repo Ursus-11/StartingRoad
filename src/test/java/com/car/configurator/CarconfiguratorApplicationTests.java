caf65bb9e3e9dbed866117ca104a9df9ec8a1486
Ursus-11 <6per.aspera.ad.astra9@gmail.com>
2022-07-31 22:54:36 +0300

    created new project

---
+++ src/test/java/com/car/configurator/CarconfiguratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.car.configurator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarconfiguratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
